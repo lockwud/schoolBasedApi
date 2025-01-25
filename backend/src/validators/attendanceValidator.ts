3b61976a22e1db8250d3b1f09e92387f0e324a78
lockwud <oklement3@gmail.com>
2025-01-25 16:11:25 +0000

    fix bug in date validator 3

--- backend/src/validators/attendanceValidator.ts
+++ backend/src/validators/attendanceValidator.ts
@@ -8,24 +8,28 @@ export const attendanceStatus = z.enum([
 
 
 export const attendanceSchema = z.object({
-    studentId: z.string({
-        required_error: "Please provide student Id"
-    }).trim().min(10, "Invalid Index"),
-    date: 
-        z.date({
-          required_error: "Please provide a valid date",
-        }).min(new Date(2023, 0, 1), "Date cannot be before January 1, 2023")
-        .max(new Date(), "Date cannot be in the future"),
+    studentId: z
+      .string({ required_error: "Please provide student Id" })
+      .trim()
+      .min(10, "Invalid Index"),
+    date: z.preprocess(
+      (arg) => {
+        if (typeof arg === "string") {
+          const parsedDate = new Date(arg);
+          return isNaN(parsedDate.getTime()) ? null : parsedDate;
+        }
+        return arg;
+      },
+      z.date({
+        required_error: "Please provide a valid date",
+      }).min(new Date(2022, 0, 1), "Date cannot be before January 1, 2022")
+       .max(new Date(), "Date cannot be in the future")
+    ),
     attendanceStatus: attendanceStatus,
     reason: z.string().optional(),
-    classId: z.string({
-        required_error: "Please provide the class Id"
-    }),
-    tutorId: z.string({
-        required_error: "Please provide the tutor Id"
-    })
+    classId: z.string({ required_error: "Please provide the class Id" }),
+    tutorId: z.string({ required_error: "Please provide the tutor Id" }),
+  });
 
-});
-
-
-export type attendanceDto = z.infer<typeof attendanceSchema>
+  export type attendanceDto = z.infer<typeof attendanceSchema>
+  
