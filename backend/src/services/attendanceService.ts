0cdc95656180d71e5a5b8718ee9e6c8bda725c4b
lockwud <oklement3@gmail.com>
2025-01-25 16:17:32 +0000

    fix bug in date validator 4

--- backend/src/services/attendanceService.ts
+++ backend/src/services/attendanceService.ts
@@ -28,7 +28,7 @@ export const createAttendance = async (attendanceData: attendanceDto | attendanc
     where: {
       OR: dataArray.map((data) => ({
         studentId: data.studentId,
-        date: data.date.toISOString(),
+        date: data.date,
       })),
     },
   });
