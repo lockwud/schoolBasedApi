af9a7edd7cdd83265d575facd7cde695201ef68d
lockwud <oklement3@gmail.com>
2025-01-26 17:54:56 +0000

    Fixed bug

--- backend/src/routes/web/analyticsRoute.ts
+++ backend/src/routes/web/analyticsRoute.ts
@@ -1,26 +1,11 @@
 import { Router } from "express";
-import * as analytics from "../../controllers/analyticsController"
+import {analyticsController} from "../../controllers/analyticsController"
 
 const analyticsRoute = Router();
 
-analyticsRoute.get("/totalStudents",
-     analytics.fetchTotalStudents
-);
 
-analyticsRoute.get("/studentsByGender",
-     analytics.fetchStudentsByGender
-);
+// Single route for all analytics
+analyticsRoute.get("/", analyticsController.getAllAnalytics);
 
-analyticsRoute.get("/topPerforming",
-     analytics.fetchTopPerformingStudent
-);
-
-analyticsRoute.get("/totalTutors",
-     analytics.fetchTotalTutors
-);
-
-analyticsRoute.get("/population",
-     analytics.fetchTotalPopulation
-)
 
 export default analyticsRoute;
