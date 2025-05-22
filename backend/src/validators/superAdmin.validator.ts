374314070f71f2e41c60d8c7c5d4ca8a2ee24110
lockwud <oklement3@gmail.com>
2025-05-22 02:30:46 +0000

    fixing zod validation error for superadmin password signUp

--- backend/src/validators/superAdmin.validator.ts
+++ backend/src/validators/superAdmin.validator.ts
@@ -4,9 +4,8 @@ export const superAdminSchema = z.object({
     fullname: z.string({required_error: "Full name is required"}).trim().min(3, "Full name should be at least 3 characters long"),
     email: z.string().email({message: "Invalid email address"}),
     password: z.string({required_error: "Password is required"}).min(8, "Password should be at least 8 characters long")
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Password should contain at least one uppercase letter, one lowercase letter, one number, and one special character")
     .trim()
-    .min(1, "Invslid password format"),
+    .min(1, "Invalid password format"),
     phone: z.string({required_error: "Phone number is required"}).trim().min(10, "Phone number should be at least 10 digits long").max(13, "Phone number should be at most 13 digits long"),
     status: z.enum(["active", "inactive", "deleted"]),
     otp: z.string({required_error: "Please enter otp"}).optional(),
